9bcf922cb2e5f3fa0cd8dce5a105e7fd4f590bd5
Valdas Stonkus <mr.valdas.stonkus@gmail.com>
2019-10-26 17:03:44 +0300

    Add default board

---
+++ src/default-board.js
@@ -0,0 +1,74 @@
+export default {
+  name: "workshop",
+  columns: [
+    {
+      name: "todo",
+      tasks: [
+        {
+          id: "1",
+          name: "first task",
+          description: "",
+          userAssigned: null
+        },
+        {
+          id: "2",
+          name: "second task",
+          description: "",
+          userAssigned: null
+        },
+        {
+          id: "3",
+          name: "thrid task",
+          description: "",
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: "in-progress",
+      tasks: [
+        {
+          id: "4",
+          name: "first task",
+          description: "",
+          userAssigned: null
+        },
+        {
+          id: "5",
+          name: "second task",
+          description: "",
+          userAssigned: null
+        },
+        {
+          id: "6",
+          name: "thrid task",
+          description: "",
+          userAssigned: null
+        }
+      ]
+    },
+    {
+      name: "done",
+      tasks: [
+        {
+          id: "7",
+          name: "first task",
+          description: "",
+          userAssigned: null
+        },
+        {
+          id: "8",
+          name: "second task",
+          description: "",
+          userAssigned: null
+        },
+        {
+          id: "9",
+          name: "thrid task",
+          description: "",
+          userAssigned: null
+        }
+      ]
+    }
+  ]
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="home">
-    <span>This is home page</span>
+  <div>
+    <!-- Action bar -->
+    <v-card
+      class="d-flex my-3 headline font-weight-bold"
+      color="rgb(0,0,0,0)"
+      flat
+      height="36"
+    >
+      <input
+        class="fix mr-2 pa-5 headline font-weight-bold"
+        type="text"
+        v-autowidth="{ maxWidth: '960px', minWidth: '20px', comfortZone: 0 }"
+        v-model="name"
+      />
+      <v-btn>Add Deal</v-btn>
+    </v-card>
+
+    <!-- List columns ------------ -->
+    <v-card class="d-flex mb-6" color="rgb(0,0,0,0)" flat>
+      <v-card
+        v-for="column in board.columns"
+        :key="column"
+        class="pa-2 ms-2"
+        width="272"
+        color="#ebecf0"
+      >
+        <v-card-title>{{ column.name }}</v-card-title>
+
+        <!-- list of deals -->
+        <v-card v-for="deal in column.tasks" :key="deal.id">
+          <v-card class="my-2 pa-2">{{ deal.name }}</v-card>
+        </v-card>
+      </v-card>
+    </v-card>
+
+    <!-- --------------------- -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { mapState } from "vuex";
 export default {
   name: "home",
-  components: {}
+  data() {
+    return {
+      name: "Company name"
+    };
+  },
+  components: {},
+  computed: mapState(["board"])
 };
 </script>
